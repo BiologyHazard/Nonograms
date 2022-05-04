4f52069fe0b70cc9bf9fe5ff311173d1ed4ff4f4
BioHazard <3482991796@qq.com>
2022-05-04 18:04:57 +0800

    typing

--- nonograms.py
+++ nonograms.py
@@ -1,4 +1,6 @@
-from io import TextIOWrapper
+# from io import TextIOWrapper
+from typing import IO
+import sys
 import numpy as np
 import time
 
@@ -13,7 +15,21 @@ print_chars = {
 }
 
 
-def solve_a_single_line(line, hint):
+class EmptyFile:
+    def write(*para, **kw):
+        pass
+
+    def writeline(*para, **kw):
+        pass
+
+    def writelines(*para, **kw):
+        pass
+
+    def close(*para, **kw):
+        pass
+
+
+def solve_a_single_line(line: list[int], hint: list[int]) -> tuple[np.ndarray, bool] | None:
     def can_insert_color(idx, x, color):  # 从idx开始的x格插入color不冲突
         if idx + x > line_size:
             return False
@@ -22,7 +38,7 @@ def solve_a_single_line(line, hint):
                 return False
         return True
 
-    def can_fill(idx, group):  # 从 第idx个格子, 第group组黑块 开始的子问题 是否有解
+    def can_fill(idx: int, group: int):  # 从 第idx个格子, 第group组黑块 开始的子问题 是否有解
         if calc[idx][group] != 0:
             return bool(calc[idx][group] - 1)
 
@@ -74,7 +90,7 @@ def solve_a_single_line(line, hint):
     return result, solved
 
 
-def solve(map, height, width, x_hints, y_hints, logf=None):
+def solve(map: np.ndarray, height: int, width: int, x_hints: list[list[int]], y_hints: list[list[int]], logf: IO[str] | EmptyFile = EmptyFile()):
     condition_changed = True
     solved_x = np.zeros(height, dtype=np.bool_)
     solved_y = np.zeros(width, dtype=np.bool_)
@@ -87,8 +103,8 @@ def solve(map, height, width, x_hints, y_hints, logf=None):
                 hint = x_hints[i]
                 temp = solve_a_single_line(line, hint)
                 if temp is None:
-                    # logf.write(
-                    #     f'row {i} (hint:{hint}) {print_line(line)} ERROR!!\n\n')
+                    logf.write(
+                        f'row {i} (hint:{hint}) {print_line(line)} ERROR!!\n\n')
                     return None
                 line_solved, solved = temp
                 if solved:
@@ -96,15 +112,15 @@ def solve(map, height, width, x_hints, y_hints, logf=None):
                 if not (line == line_solved).all():  # line与line_solved有任一值不等
                     condition_changed = True
                     map[i] = line_solved
-                # logf.write(
-                #     f'row {i} (hint:{hint}) from {print_line(line)} to {print_line(line_solved)}\n')
-            # else:
-                # logf.write(
-                #     f'row {i} (hint:{hint}) {print_line(map[i])} solved\n')
+                logf.write(
+                    f'row {i} (hint:{hint}) from {print_line(line)} to {print_line(line_solved)}\n')
+            else:
+                logf.write(
+                    f'row {i} (hint:{hint}) {print_line(map[i])} solved\n')
 
-        # for line in map:
-            #     logf.write(print_line(line) + '\n')
-            # logf.write('\n')
+            for line in map:
+                logf.write(print_line(line) + '\n')
+            logf.write('\n')
 
         for i in range(width):
             if not solved_y[i]:
@@ -112,8 +128,8 @@ def solve(map, height, width, x_hints, y_hints, logf=None):
                 hint = y_hints[i]
                 temp = solve_a_single_line(line, hint)
                 if temp is None:
-                    # logf.write(
-                    #     f'col {i} (hint:{hint}) {print_line(line)} ERROR!!\n\n')
+                    logf.write(
+                        f'col {i} (hint:{hint}) {print_line(line)} ERROR!!\n\n')
                     return None
                 line_solved, solved = temp
                 if solved:
@@ -121,15 +137,15 @@ def solve(map, height, width, x_hints, y_hints, logf=None):
                 if not (line == line_solved).all():  # line与line_solved有任一值不等
                     condition_changed = True
                     map[..., i] = line_solved
-                # logf.write(
-                #     f'col {i} (hint:{hint}) from {print_line(line)} to {print_line(line_solved)}\n')
-            # else:
-            #     logf.write(
-            #         f'col {i} (hint:{hint}) {print_line(map[..., i])} solved\n')
+                logf.write(
+                    f'col {i} (hint:{hint}) from {print_line(line)} to {print_line(line_solved)}\n')
+            else:
+                logf.write(
+                    f'col {i} (hint:{hint}) {print_line(map[..., i])} solved\n')
 
-        # for line in map:
-        #     logf.write(print_line(line) + '\n')
-        # logf.write('\n\n')
+        for line in map:
+            logf.write(print_line(line) + '\n')
+        logf.write('\n\n')
 
         if solved_x.all() and solved_y.all():  # 如果解完了
             return map
@@ -152,7 +168,7 @@ def solve(map, height, width, x_hints, y_hints, logf=None):
     return None
 
 
-def print_line(line, print_chars=print_chars):
+def print_line(line, print_chars: dict[int, str] = print_chars):
     l = []
     for i in line:
         l.append(print_chars[i])
@@ -160,12 +176,18 @@ def print_line(line, print_chars=print_chars):
 
 
 class Nonograms:
-    def __init__(self, file=None, height=None, width=None, x_hints=None, y_hints=None):
+    def __init__(self,
+                 file: str | IO[str] | None = None,
+                 height: int | None = None,
+                 width: int | None = None,
+                 x_hints: list[list[int]] | None = None,
+                 y_hints: list[list[int]] | None = None):
+
         if file is not None:
             if isinstance(file, str):
                 with open(file, 'r', encoding='utf-8') as f:
                     self.load_from_file(f)
-            elif isinstance(file, TextIOWrapper):
+            elif hasattr(file, 'readlines'):
                 self.load_from_file(file)
             else:
                 raise TypeError
@@ -175,7 +197,7 @@ class Nonograms:
     # def __len__(self):
     #     return self.height * self.width
 
-    def load_from_file(self, map_file: TextIOWrapper):
+    def load_from_file(self, map_file: IO[str]) -> None:
         hints = map_file.readlines()
         hints = [hint.strip() for hint in hints if len(hint.strip()) > 0]
         for i, hint in enumerate(hints):
@@ -189,7 +211,7 @@ class Nonograms:
         y_hint = hints[self.height + 1:]
         self.y_hints = [[int(x) for x in hint.split()] for hint in y_hint]
 
-    def init_game(self, height=None, width=None, x_hints=None, y_hints=None):
+    def init_game(self, height: int, width: int, x_hints: list[list[int]], y_hints: list[list[int]]) -> None:
         self.height = height
         self.width = width
         self.map = np.empty((height, width), dtype=np.int8)
@@ -199,17 +221,17 @@ class Nonograms:
         if (len(self.x_hints) != self.height) or (len(self.y_hints) != self.width):
             raise ValueError
 
-    def get_row(self, x):
+    def get_row(self, x: int):
         return self.map[x]
 
-    def get_col(self, y):
+    def get_col(self, y: int):
         return self.map[..., y]
 
-    def print(self, print_chars=print_chars):
+    def print(self, print_chars: dict[int, str] = print_chars, file=None):
         for line in self.map:
-            print(print_line(line))
+            print(print_line(line, print_chars=print_chars), file=file)
 
-    def solve(self, logf=None):
+    def solve(self, logf: IO[str] | EmptyFile = EmptyFile()):
         self.map = solve(self.map,
                          self.height,
                          self.width,
@@ -220,11 +242,24 @@ class Nonograms:
 
 
 if __name__ == '__main__':
-    time0 = time.time()
-    with open('solve.log', 'w', encoding='utf-8') as logf:
-        nonograms = Nonograms('game_map.txt')
-        nonograms.solve(logf)
-        nonograms.print()
-        # print(solve_one_line(np.array([0, 0, 1, 1, 0]), [3]))
-        # print(solve_one_line(np.zeros(50, np.int8), [3, 4, 1, 6, 1, 3, 2, 12, 1]))
-    print(f'time used {time.time() - time0:.3f}s')
+    supported_argv = ['-l', '--log']
+    lowered_argv = list(map(str.lower, sys.argv))
+    logf = EmptyFile()
+    for s in supported_argv:
+        try:
+            idx = lowered_argv.index(s)
+            try:
+                logf = open(sys.argv[idx+1], 'w', encoding='utf-8')
+            except:
+                logf = sys.stdout
+        except:
+            pass
+
+    nonograms = Nonograms('game_map.txt')
+    start_time = time.time()
+    nonograms.solve(logf)
+    nonograms.print()
+    print(f'solve time used {time.time() - start_time:.3f}s')
+
+    # print(solve_one_line(np.array([0, 0, 1, 1, 0]), [3]))
+    # print(solve_one_line(np.zeros(50, np.int8), [3, 4, 1, 6, 1, 3, 2, 12, 1]))
